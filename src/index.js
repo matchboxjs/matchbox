4b5acf7c3f5d115a938617bfd2ededbbfc5fa0e3
tunderdomb <tunderdomb@gmail.com>
2015-10-21 13:26:06 +0000

    refactor

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var matchbox = module.exports = {}
+
+matchbox.factory = require("matchbox-factory")
+matchbox.ui = require("matchbox-ui")
+matchbox.dom = require("matchbox-dom")
+matchbox.attributes = require("matchbox-attributes")
+matchbox.collections = require("matchbox-collections")
+matchbox.radio = require("matchbox-radio")
+matchbox.relay = require("matchbox-relay")
