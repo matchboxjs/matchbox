caf00cbec1e86c59433cf0725e0879d6ecb316bf
tunderdomb <tunderdomb@gmail.com>
2015-10-21 13:26:57 +0000

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var matchbox = module.exports = {}
-
-matchbox.factory = require("./src/factory")
-matchbox.ui = require("./src/ui")
-matchbox.dom = require("./src/dom")
-matchbox.attributes = require("./src/attributes")
+module.exports = require("./src")
--- test/factory.js
+++ test/factory.js
@@ -1,11 +1,9 @@
 var chai = require("chai")
 var assert = chai.assert
-var factory = require("../src/factory")
-var Role = require("../src/ui/Role")
+var factory = require("../src/node_modules/matchbox-factory")
 
 describe("factory", function () {
   it("works", function () {
-    var role = new Role()
-    assert.isTrue(true)
+
   })
 })
