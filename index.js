0e62ba77c195fa831bf49b8e9975e8abb5058810
tunderdomb <tunderdomb@gmail.com>
2015-11-24 15:55:09 +0100

    base modules

---
+++ index.js
@@ -0,0 +1,8 @@
+var matchbox = module.exports = {}
+
+matchbox.util = require("matchbox-util")
+matchbox.factory = require("matchbox-factory")
+matchbox.dom = require("matchbox-dom")
+matchbox.ui = require("matchbox-ui")
+matchbox.relay = require("matchbox-relay")
+matchbox.Radio = require("matchbox-radio")
