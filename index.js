d751ec18ca00e5db1286e3154ed99c47b933ef9b
tunderdomb <tunderdomb@gmail.com>
2016-03-10 17:26:31 +0100

    updated main script

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var matchbox = module.exports = {}
 
-matchbox.util = require("matchbox-util")
-matchbox.factory = require("matchbox-factory")
-matchbox.dom = require("matchbox-dom")
-matchbox.ui = require("matchbox-ui")
-matchbox.relay = require("matchbox-relay")
-matchbox.Radio = require("matchbox-radio")
+matchbox.config = require("./src/config")
+matchbox.commands = {
+  addGen: require("./bin/commands/add-gen"),
+  bootstrap: require("./bin/commands/bootstrap"),
+  generate: require("./bin/commands/generate"),
+  init: require("./bin/commands/init")
+}
