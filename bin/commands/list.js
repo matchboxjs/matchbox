383e3a4abdbcdadeb6a211e0650b003e493a6179
tunderdomb <tunderdomb@gmail.com>
2016-03-22 17:58:19 +0100

    added host to list results

--- bin/commands/list.js
+++ bin/commands/list.js
@@ -4,21 +4,26 @@ var logger = hideout.util.logger
 var config = require("../../src/config")
 
 module.exports = function() {
-  var glob = config.resolveHostPath("node_modules/*", config.RC_FILE_GLOB)
-  return hideout
-    .fs.src(glob)
-    .then(function(files) {
-      return files.map(function(file) {
-        return path.basename(path.dirname(file))
-      })
-    })
-    .then(function(packages) {
-      return hideout.flow.series(packages, function(pkg) {
-        return config.package(pkg).then(function(rc) {
-          var generatorList = "(" + Object.keys(rc.generators||{}).join(", ") + ")"
-          logger.info(logger.format.label(pkg), logger.format.comment(generatorList))
+  function display(rc) {
+    var generatorList = "(" + Object.keys(rc.generators||{}).join(", ") + ")"
+    logger.info(logger.format.label(rc.name), logger.format.comment(generatorList))
+  }
+  config()
+    .then(display)
+    .then(function() {
+      var packageGlob = config.resolveHostPath("node_modules/*", config.RC_FILE_GLOB)
+      return hideout
+        .fs.src(packageGlob)
+        .then(function(files) {
+          return files.map(function(file) {
+            return path.basename(path.dirname(file))
+          })
         })
-      })
+        .then(function(packages) {
+          return hideout.flow.series(packages, function(pkg) {
+            return config.package(pkg).then(display)
+          })
+        })
     })
     .catch(logger.stack)
 }
